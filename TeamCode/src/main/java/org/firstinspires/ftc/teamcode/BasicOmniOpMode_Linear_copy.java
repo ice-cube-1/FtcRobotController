0117f7dd723036ef9b8e43d61ffd27e44ec621f3
ice-cube-1 <alice.goward@spgs.org>
2024-11-07 18:43:05 +0000

    datalogger stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicOmniOpMode_Linear_copy.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicOmniOpMode_Linear_copy.java
@@ -29,40 +29,12 @@
 
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-/*
- * This file contains an example of a Linear "OpMode".
- * An OpMode is a 'program' that runs in either the autonomous or the teleop period of an FTC match.
- * The names of OpModes appear on the menu of the FTC Driver Station.
- * When a selection is made from the menu, the corresponding OpMode is executed.
- *
- * This particular OpMode illustrates driving a 4-motor Omni-Directional (or Holonomic) robot.
- * This code will work with either a Mecanum-Drive or an X-Drive train.
- * Both of these drives are illustrated at https://gm0.org/en/latest/docs/robot-design/drivetrains/holonomic.html
- * Note that a Mecanum drive must display an X roller-pattern when viewed from above.
- *
- * Also note that it is critical to set the correct rotation direction for each motor.  See details below.
- *
- * Holonomic drives provide the ability for the robot to move in three axes (directions) simultaneously.
- * Each motion axis is controlled by one Joystick axis.
- *
- * 1) Axial:    Driving forward and backward               Left-joystick Forward/Backward
- * 2) Lateral:  Strafing right and left                     Left-joystick Right and Left
- * 3) Yaw:      Rotating Clockwise and counter clockwise    Right-joystick Right and Left
- *
- * This code is written assuming that the right-side motors need to be reversed for the robot to drive forward.
- * When you first test your robot, if it moves backward when you push the left stick forward, then you must flip
- * the direction of all 4 motors (see code below).
- *
- * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
- * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
- */
+import java.util.Random;
 
 @TeleOp(name="Basic: Omni Linear OpMode", group="Linear OpMode")
 public class BasicOmniOpMode_Linear_copy extends LinearOpMode {
@@ -73,69 +45,42 @@ public class BasicOmniOpMode_Linear_copy extends LinearOpMode {
     private DcMotor leftBackDrive = null;
     private DcMotor rightFrontDrive = null;
     private DcMotor rightBackDrive = null;
+    Datalog datalog;
+    Random random = new Random();
 
     @Override
     public void runOpMode() {
-
-        // Initialize the hardware variables. Note that the strings used here must correspond
-        // to the names assigned during the robot configuration step on the DS or RC devices.
         leftFrontDrive  = hardwareMap.get(DcMotor.class, "left_front_drive");
         leftBackDrive  = hardwareMap.get(DcMotor.class, "left_back_drive");
         rightFrontDrive = hardwareMap.get(DcMotor.class, "right_front_drive");
         rightBackDrive = hardwareMap.get(DcMotor.class, "right_back_drive");
-
-        // ########################################################################################
-        // !!!            IMPORTANT Drive Information. Test your motor directions.            !!!!!
-        // ########################################################################################
-        // Most robots need the motors on one side to be reversed to drive forward.
-        // The motor reversals shown here are for a "direct drive" robot (the wheels turn the same direction as the motor shaft)
-        // If your robot has additional gear reductions or uses a right-angled drive, it's important to ensure
-        // that your motors are turning in the correct direction.  So, start out with the reversals here, BUT
-        // when you first test your robot, push the left joystick forward and observe the direction the wheels turn.
-        // Reverse the direction (flip FORWARD <-> REVERSE ) of any wheel that runs backward
-        // Keep testing until ALL the wheels move the robot forward when you push the left joystick forward.
         leftFrontDrive.setDirection(DcMotor.Direction.REVERSE);
         leftBackDrive.setDirection(DcMotor.Direction.REVERSE);
         rightFrontDrive.setDirection(DcMotor.Direction.FORWARD);
-        rightBackDrive.setDirection(DcMotor.Direction.FORWARD);
 
-        // Wait for the game to start (driver presses START)
+        datalog = new Datalog("datalog"+String.format("%02d",random.nextInt(100)));
+
+
         telemetry.addData("Status", "Initialized");
         telemetry.update();
 
         waitForStart();
         runtime.reset();
 
-        // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
             leftFrontDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
             leftBackDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
             rightFrontDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
             rightBackDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-            telemetry.addData("Starting at",  "%7d %7d %7d %7d",
-                    leftFrontDrive.getCurrentPosition(),
-                    leftBackDrive.getCurrentPosition(),
-                    rightFrontDrive.getCurrentPosition(),
-                    rightBackDrive.getCurrentPosition()
-            );
-            telemetry.update();
 
             double max;
-
-            // POV Mode uses left joystick to go forward & strafe, and right joystick to rotate.
-            double axial   = -gamepad1.left_stick_y;  // Note: pushing stick forward gives negative value
+            double axial   = -gamepad1.left_stick_y;
             double lateral =  gamepad1.left_stick_x;
             double yaw     =  gamepad1.right_stick_x;
-
-            // Combine the joystick requests for each axis-motion to determine each wheel's power.
-            // Set up a variable for each drive wheel to save the power level for telemetry.
             double leftFrontPower  = axial + lateral + yaw;
             double rightFrontPower = axial - lateral - yaw;
             double leftBackPower   = axial - lateral + yaw;
             double rightBackPower  = axial + lateral - yaw;
-
-            // Normalize the values so no wheel power exceeds 100%
-            // This ensures that the robot maintains the desired motion.
             max = Math.max(Math.abs(leftFrontPower), Math.abs(rightFrontPower));
             max = Math.max(max, Math.abs(leftBackPower));
             max = Math.max(max, Math.abs(rightBackPower));
@@ -146,29 +91,55 @@ public class BasicOmniOpMode_Linear_copy extends LinearOpMode {
                 leftBackPower   /= max;
                 rightBackPower  /= max;
             }
-
-            // This is test code:
-            //
-            // Uncomment the following code to test your motor directions.
-            // Each button should make the corresponding motor run FORWARD.
-            //   1) First get all the motors to take to correct positions on the robot
-            //      by adjusting your Robot Configuration if necessary.
-            //   2) Then make sure they run in the correct direction by modifying the
-            //      the setDirection() calls above.
-            // Once the correct motors move in the correct direction re-comment this code.
-
-            /*
-            leftFrontPower  = gamepad1.x ? 1.0 : 0.0;  // X gamepad
-            leftBackPower   = gamepad1.a ? 1.0 : 0.0;  // A gamepad
-            rightFrontPower = gamepad1.y ? 1.0 : 0.0;  // Y gamepad
-            rightBackPower  = gamepad1.b ? 1.0 : 0.0;  // B gamepad
-            */
-
-            // Send calculated power to wheels
             leftFrontDrive.setPower(leftFrontPower);
             rightFrontDrive.setPower(rightFrontPower);
             leftBackDrive.setPower(leftBackPower);
             rightBackDrive.setPower(rightBackPower);
 
+            telemetry.addData("Starting at",  "%7d %7d %7d %7d",
+                    leftFrontDrive.getCurrentPosition(),
+                    leftBackDrive.getCurrentPosition(),
+                    rightFrontDrive.getCurrentPosition(),
+                    rightBackDrive.getCurrentPosition()
+            );
+            telemetry.update();
+            datalog.leftFrontEncoder.set(leftFrontDrive.getCurrentPosition());
+            datalog.leftBackEncoder.set(leftFrontDrive.getCurrentPosition());
+            datalog.rightFrontEncoder.set(rightFrontDrive.getCurrentPosition());
+            datalog.rightBackEncoder.set(rightBackDrive.getCurrentPosition());
+            datalog.leftFrontPower.set(leftFrontPower);
+            datalog.leftBackPower.set(leftBackPower);
+            datalog.rightFrontPower.set(rightFrontPower);
+            datalog.rightBackPower.set(rightBackPower);
+            datalog.writeLine();
         }
-    }}
+    }
+    public static class Datalog
+    {
+        private final Datalogger datalogger;
+        public Datalogger.GenericField leftFrontEncoder = new Datalogger.GenericField("LeftFrontEncoder");
+        public Datalogger.GenericField leftBackEncoder = new Datalogger.GenericField("LeftBackEncoder");
+        public Datalogger.GenericField rightFrontEncoder = new Datalogger.GenericField("RightFrontEncoder");
+        public Datalogger.GenericField rightBackEncoder = new Datalogger.GenericField("RightBackEncoder");
+        public Datalogger.GenericField leftFrontPower = new Datalogger.GenericField("LeftFrontPower");
+        public Datalogger.GenericField leftBackPower = new Datalogger.GenericField("LeftBackPower");
+        public Datalogger.GenericField rightFrontPower = new Datalogger.GenericField("RightFrontPower");
+        public Datalogger.GenericField rightBackPower = new Datalogger.GenericField("RightBackPower");
+
+        public Datalog(String name)
+        {
+            datalogger = new Datalogger.Builder()
+                    .setFilename(name)
+                    .setAutoTimestamp(Datalogger.AutoTimestamp.DECIMAL_SECONDS)
+                    .setFields(
+                            leftFrontEncoder, leftBackEncoder, rightFrontEncoder, rightBackEncoder,
+                            leftFrontPower, leftBackPower,rightFrontPower, rightBackPower
+                    )
+                    .build();
+        }
+        public void writeLine()
+        {
+            datalogger.writeLine();
+        }
+    }
+}
