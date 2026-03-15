d0a20a95a008380063450e8750436f3d41952df0
ice-cube-1 <alice.goward@spgs.org>
2026-03-15 14:32:53 +0000

    better wraparound logic

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TurretTest.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TurretTest.kt
@@ -8,11 +8,13 @@ import com.qualcomm.robotcore.util.ElapsedTime
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName
 import org.firstinspires.ftc.teamcode.Constants.CCW_TURRET
 import org.firstinspires.ftc.teamcode.Constants.CW_TURRET
+import org.firstinspires.ftc.teamcode.Constants.ENCODER_ERROR
 import org.firstinspires.ftc.teamcode.Constants.TURRET_ENCODER_KP
 import org.firstinspires.ftc.teamcode.Constants.TURRET_SPEED
 import org.firstinspires.ftc.vision.VisionPortal
 import org.firstinspires.ftc.vision.apriltag.AprilTagGameDatabase
 import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor
+import kotlin.math.abs
 
 private enum class Turret {SCANNING, DETECTED, WRAPPING}
 
@@ -27,7 +29,7 @@ class TurretTest : LinearOpMode() {
     private var mostRecent = 0.0
     private var timer = ElapsedTime()
     private var turretState = Turret.SCANNING
-    private var goto = 0
+    private var goto = CCW_TURRET
     private var scanMin = 0.0
     private var scanMax = 0.0
     private var toScanMin = true
@@ -40,34 +42,28 @@ class TurretTest : LinearOpMode() {
             .addProcessor(aprilTag)
             .build()
         FtcDashboard.getInstance().startCameraStream(visionPortal, 0.0)
-        setScanBounds(-45.0,45.0)
+        setScanBounds(135.0,-135.0)
         waitForStart()
         turret.resetEncoder()
         while (opModeIsActive()) {
+            checkWraparound()
             when (turretState) {
                 Turret.SCANNING -> {
-                    if (timer.milliseconds() < mostRecent + 500) {
-                        turretState = Turret.DETECTED
-                    } else if (!checkWraparound()) {
-                        scan()
-                    }
+                    if (timer.milliseconds() < mostRecent + 500) turretState = Turret.DETECTED
+                    else scan()
                 }
                 Turret.DETECTED -> {
-                    if (timer.milliseconds() > mostRecent + 500) {
-                        turretState = Turret.SCANNING
-                    } else if (!checkWraparound()) {
-                        centerOnTag()
-                    }
+                    if (timer.milliseconds() > mostRecent + 500) turretState = Turret.SCANNING
+                    else centerOnTag()
                 }
                 Turret.WRAPPING -> {
                     if (timer.milliseconds() < mostRecent + 500) {
                         turretState = Turret.DETECTED
-                    } else if (!checkWraparound()) {
-                        wrap()
-                    }
+                    } else wrap()
                 }
             }
             telemetry.addData("turret state", turretState)
+            telemetry.addData("goto (wrap)", goto)
             telemetry.addData("pos", turret.getPosition())
             lookForTag()
             telemetry.update()
@@ -88,14 +84,23 @@ class TurretTest : LinearOpMode() {
         else { return false }
         return true
     }
-    private fun wrap() { turret.setPower(if (goto < turret.getPosition()) -TURRET_SPEED else TURRET_SPEED) }
+    private fun wrap() {
+        val error = goto - turret.getPosition()
+        if (abs(error) < ENCODER_ERROR) {
+            turret.setPower(0.0)
+            turretState = Turret.SCANNING
+        } else {
+            turret.setPower(if (error > 0) TURRET_SPEED else -TURRET_SPEED)
+        }
+    }
     private fun centerOnTag() { turret.setPower(-lastAngle * TURRET_ENCODER_KP) }
     private fun scan() {
         if (scanMin < scanMax) {
-            if ((toScanMin && scanMin > turret.getPosition()) || (!toScanMin && scanMax < turret.getPosition())) {
-                toScanMin = !toScanMin
-            }
+            if ((toScanMin && scanMin > turret.getPosition()) || (!toScanMin && scanMax < turret.getPosition())) { toScanMin = !toScanMin }
             turret.setPower(if (toScanMin) -TURRET_SPEED else TURRET_SPEED)
+        } else {
+            turretState = Turret.WRAPPING
+            goto = if (goto == CCW_TURRET) CW_TURRET else CCW_TURRET
         }
     }
     private fun lookForTag() {
