af0bd4f3398546c618df84b726a651a2c5e05b6d
ice-cube-1 <alice.goward@spgs.org>
2026-04-07 12:01:16 +0100

    limelight testing and shooter testing

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LimeLightTest.kt
@@ -0,0 +1,37 @@
+package org.firstinspires.ftc.teamcode
+
+import com.qualcomm.hardware.limelightvision.Limelight3A
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp
+import java.lang.Math.toDegrees
+import kotlin.math.atan
+import kotlin.math.sqrt
+
+
+@TeleOp
+class LimeLightTest: LinearOpMode() {
+    private lateinit var limelight: Limelight3A
+
+    override fun runOpMode() {
+        waitForStart()
+        limelight = hardwareMap.get(Limelight3A::class.java, "limelight")
+        limelight.setPollRateHz(100)
+        limelight.start()
+        limelight.pipelineSwitch(0)
+        while (opModeIsActive()) {
+            val result = limelight.latestResult
+            if (result != null && result.isValid) {
+                val fiducials = result.fiducialResults
+
+                for (fiducial in fiducials) {
+                    val pose = fiducial.targetPoseCameraSpace
+
+                    telemetry.addData("Tag ID", fiducial.fiducialId)
+                    telemetry.addData("Range (m)", sqrt(pose.position.x * pose.position.x + pose.position.z * pose.position.z))
+                    telemetry.addData("bearing", toDegrees(atan(pose.position.x / pose.position.z)))
+                }
+                telemetry.update()
+            }
+        }
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ShooterNew.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ShooterNew.kt
@@ -1,43 +1,39 @@
 package org.firstinspires.ftc.teamcode
 
-import com.acmerobotics.dashboard.FtcDashboard
-import com.qualcomm.robotcore.hardware.AnalogInput
+import com.qualcomm.hardware.limelightvision.Limelight3A
 import com.qualcomm.robotcore.hardware.DcMotorSimple
 import com.qualcomm.robotcore.hardware.HardwareMap
+import com.qualcomm.robotcore.hardware.PwmControl
 import com.qualcomm.robotcore.hardware.Servo
+import com.qualcomm.robotcore.hardware.ServoImplEx
 import com.qualcomm.robotcore.util.ElapsedTime
-import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName
-import org.firstinspires.ftc.teamcode.Constants.ADD_TEST
 import org.firstinspires.ftc.teamcode.Constants.CCW_TURRET
 import org.firstinspires.ftc.teamcode.Constants.CW_TURRET
-import org.firstinspires.ftc.teamcode.Constants.SERVO_ERROR
 import org.firstinspires.ftc.teamcode.Constants.HOOD_ANGLE
 import org.firstinspires.ftc.teamcode.Constants.KP_SHOOTER
-import org.firstinspires.ftc.teamcode.Constants.TURRET_KP
 import org.firstinspires.ftc.teamcode.Constants.TURRET_STEP
 import org.firstinspires.ftc.teamcode.Constants.VELOCITY_DELTA
-import org.firstinspires.ftc.vision.VisionPortal
-import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor
+import java.lang.Math.toDegrees
 import kotlin.math.abs
+import kotlin.math.atan2
 import kotlin.math.max
 import kotlin.math.min
+import kotlin.math.sqrt
 
 
 enum class TurretState {DETECTED, WRAPPING}
 
-class ShooterNew(hardwareMap: HardwareMap) {
-    private val aprilTag = AprilTagProcessor.easyCreateWithDefaults()
-    private var visionPortal: VisionPortal =
-        VisionPortal.easyCreateWithDefaults(hardwareMap.get(WebcamName::class.java, "Webcam 1"), aprilTag)
+class ShooterNew(hardwareMap: HardwareMap, private var tagID: Int) {
+    private var limelight = hardwareMap.get(Limelight3A::class.java, "limelight")
     private val turret = arrayOf(
-        hardwareMap.get(Servo::class.java, "t1"),
-        hardwareMap.get(Servo::class.java, "t2")
+        hardwareMap.get(ServoImplEx::class.java, "t1").apply {
+            pwmRange = PwmControl.PwmRange(500.0, 2500.0)
+        },
+        hardwareMap.get(ServoImplEx::class.java, "t2").apply {
+            pwmRange = PwmControl.PwmRange(500.0, 2500.0)
+        }
     )
-    private val turretRealPositions = arrayOf(
-        hardwareMap.get(AnalogInput::class.java, "t1"),
-        hardwareMap.get(AnalogInput::class.java, "t2")
-    )
-    private val hoodAngle = hardwareMap.get(Servo::class.java, "hood")
+    private val hoodAngle = hardwareMap.get(Servo::class.java, "hood").apply { position = HOOD_ANGLE }
     private var lastDist = 0.0
     private var lastAngle = 0.0
     private val motors =  arrayOf(
@@ -53,88 +49,62 @@ class ShooterNew(hardwareMap: HardwareMap) {
     private var shooterOn = false
     private var targetV = 0
     private var goto = CCW_TURRET
-    private var tagID = 20
-    private var manual = false
-    init {
-        FtcDashboard.getInstance().startCameraStream(visionPortal, 0.0)
-        hoodAngle.position = HOOD_ANGLE
-    }
-    fun setStart(tagID: Int, manual: Boolean) { this.tagID = tagID; this.manual = manual }
-    private fun lookForTag() {
-        val currentDetections = aprilTag.detections
-        for (detection in currentDetections) {
-            if (detection.id == tagID && detection.metadata != null) {
-                lastDist = detection.ftcPose.range
-                lastAngle = detection.ftcPose.bearing
-                mostRecent = timer.milliseconds()
-            }
-        }
-    }
+    private var nextPos = 0.0
     fun getMotifID(): Int {
-        val currentDetections = aprilTag.detections
-        for (detection in currentDetections) {
-            if (detection.id in 21..23) {return detection.id }
+        val currentDetections = limelight.latestResult
+        for (tag in currentDetections.fiducialResults) {
+            if (tag.fiducialId in 21..23) {return tag.fiducialId }
         }
         return -1
-    }
-    fun setTurretPos(position: Double) {
-        turret[0].position = position
-        turret[1].position = 360 - position
-    }
-    private fun getTurretPosition() : Double {
-        return turretRealPositions[0].voltage + turretRealPositions[1].voltage / 6.6 * 360
     }
 
     fun canShoot(): Boolean { return abs(lastAngle) < 3 && lastDist < 120.0 && (targetV - motors[1].getVelocity() < 30) }
     private fun getTargetVelocity(): Int {
-        return (lastDist * 5.9976 + 1022.8 + ADD_TEST).toInt()
+        return (lastDist * 5.9976 + 1022.8).toInt()
     }
-    private fun centerOnTag() {
-        if (abs(lastAngle - TURRET_KP) > 0.02) {
-            turret[0].position = lastAngle + TURRET_KP
-            turret[1].position = 360.0 - lastAngle + TURRET_KP
-        }
-    }
-    private fun wrap() {
-        if (scanTimer.milliseconds() < 500) {
-            val error = goto - getTurretPosition()
-            if (abs(error) < SERVO_ERROR) {
-                goto = if (goto == CCW_TURRET) CW_TURRET else CCW_TURRET
-            } else {
-                turret[0].position += if (error > 0) TURRET_STEP else -TURRET_STEP
-                turret[1].position -= if (error > 0) TURRET_STEP else -TURRET_STEP
-
+    private fun lookForTag() {
+        val result = limelight.latestResult
+        for (tag in result.fiducialResults) {
+            if (tag.fiducialId == tagID) {
+                mostRecent = timer.milliseconds() - result.staleness
+                val pos = tag.targetPoseCameraSpace.position
+                lastDist = sqrt(pos.x * pos.x + pos.z * pos.z)
+                lastAngle = toDegrees(atan2(pos.x , pos.z))
             }
-        } else if (scanTimer.milliseconds() > 800) { scanTimer.reset() }
+        }
     }
     fun moveTurret() {
         lookForTag()
-        checkWraparound()
         when (turretState) {
             TurretState.DETECTED -> {
                 if (timer.milliseconds() > mostRecent + 500) turretState = TurretState.WRAPPING
-                else centerOnTag()
+                else if (abs(lastAngle) > 0.02) {
+                    nextPos = lastAngle + turret[0].position * 360.0
+                }
             }
             TurretState.WRAPPING -> {
                 if (timer.milliseconds() < mostRecent + 500) turretState = TurretState.DETECTED
-                else wrap()
+                else {
+                    if (scanTimer.milliseconds() > 500) {
+                        val error = goto - turret[0].position * 360.0
+                        nextPos = (if (error > 0) TURRET_STEP else -TURRET_STEP) + turret[0].position * 360.0
+                        scanTimer.reset()
+                    } else {
+                        nextPos = turret[0].position * 360.0
+                    }
+                }
             }
         }
-    }
-    private fun checkWraparound(): Boolean {
-        if (abs(getTurretPosition() - (turret[1].position - 360.0 - turret[0].position)) > SERVO_ERROR) {
-            goto = if (goto == CW_TURRET) CCW_TURRET else CW_TURRET
-        }
-        if (getTurretPosition() > CCW_TURRET/3) {
-            turretState = TurretState.WRAPPING
-            goto = CW_TURRET
-        } else if (getTurretPosition() < CW_TURRET/3) {
-            turretState = TurretState.WRAPPING
+        if (nextPos > CW_TURRET) {
             goto = CCW_TURRET
+        } else if (nextPos < CCW_TURRET) {
+            goto = CW_TURRET
+        } else {
+            turret[0].position = nextPos / 360.0
+            turret[1].position = (360 - nextPos) / 360.0
         }
-        else { return false }
-        return true
     }
+
     fun turnOnShooter() {
         targetV = 0
         shooterOn = true
